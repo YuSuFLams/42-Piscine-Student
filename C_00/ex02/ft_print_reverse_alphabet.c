ba731191b2e4063ccc0288c7de255f7c1b4be1f4
ylamsiah <110131779+YuSuFLams@users.noreply.github.com>
2023-01-09 04:22:10 +0100

    Zoro (*_*)

---
+++ C_00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ylamsiah <ylamsiah@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/09 11:03:31 by ylamsiah          #+#    #+#             */
+/*   Updated: 2023/01/09 04:17:37 by ylamsiah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_reverse_alphabet(void)
+{
+	char i;
+
+	i = 'z' ;
+	while (i >= 'a')
+	{
+		ft_putchar(i);
+		i--;
+	}
+}
