b42f678424a875e10c5a8ab95c53ad3fec271506
ylamsiah <110131779+YuSuFLams@users.noreply.github.com>
2023-01-09 05:59:39 +0100

    Livaï (-_-)

---
+++ EXAM00/aff_first_param/aff_first_param.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   aff_first_param.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ylamsiah <ylamsiah@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/21 17:28:22 by ylamsiah          #+#    #+#             */
+/*   Updated: 2022/11/24 18:50:26 by ylamsiah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char x)
+{
+	write(1, &x, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = -1;
+	while (*(str + (++i)))
+		ft_putchar(*(str + i));
+	ft_putchar('\n');
+}
+
+int	main(int ac, char **str)
+{
+	if (ac > 1)
+		ft_putstr(str[1]);
+	else
+		ft_putchar('\n');
+	return (0);
+}
